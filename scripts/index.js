c5b7c2d38bd2e40b1c5c9d7f630a3c6befcf3ee1
Agantuk Sarkar <agantuksarkar124@gmail.com>
2024-12-31 14:45:48 +0530

    update infinite scrolling
    add: provided infinite scrolling with debouncing

--- scripts/index.js
+++ scripts/index.js
@@ -13,10 +13,10 @@ let pageNo = 1;
 let limit = 10;
 let totalItems = 0;
 let hasMoreData = true;
+let searchQuery = "";
 
 // function to fetch products and display them in UI
 async function getProductsData(pageNo, limit) {
-  
   let skip = (pageNo - 1) * limit;
 
   const productDetails = await fetchProducts(skip, limit);
@@ -30,7 +30,8 @@ async function getProductsData(pageNo, limit) {
   showProducts(productDetails.products);
 }
 
-getProductsData(pageNo, limit);
+// getProductsData(pageNo, limit);
+loadProducts(pageNo, limit);
 
 // function to show products
 function showProducts(products) {
@@ -49,42 +50,66 @@ window.addEventListener("scroll", (event) => {
     pageNo = pageNo + 1;
     limit = 10;
 
-    if (hasMoreData) {
-      getProductsData(pageNo, limit);
-    }
+    // if (hasMoreData) {
+    //   getProductsData(pageNo, limit);
+    // }
+    loadProducts(pageNo, limit);
   }
 });
 
 // to handle search inputs by adding event for search input
-searchInput.addEventListener("input",(event)=>{
-
+searchInput.addEventListener("input", (event) => {
   const searchText = event.target.value;
 
   pageNo = 1;
   limit = 10;
 
-  functionToPass(searchText,pageNo,limit);
+  functionToPass(searchText, pageNo, limit);
+  // loadProducts(pageNo,limit);
 
-  searchProducts(searchText,pageNo,limit);
+  // searchProducts(searchText, pageNo, limit);
 });
 
 // passing the function search products and time delay
-const functionToPass = debounce(searchProducts,3000);
+const functionToPass = debounce(test, 3000);
 
-// function to search products by resolving the promise
-async function searchProducts(searchText,pageNo,limit){
-
+function test(searchText) {
   productsContainer.innerHTML = "";
+  pageNo = 1;
+  limit = 10;
 
-  let skip = (pageNo - 1)* limit;
+  searchQuery = searchText;
 
-  const products = await getSearchProducts(searchText,skip,limit);
-
-  displayProducts(products.products,productsContainer);
+  loadProducts(pageNo, limit);
 }
 
+// function to load products which will decide which API should load
+function loadProducts(pageNo, limit) {
+  if (searchQuery) {
+    searchProducts(searchQuery, pageNo, limit);
+  } else {
+    getProductsData(pageNo, limit);
+  }
+}
 
+// function to search products by resolving the promise
+async function searchProducts(searchText, pageNo, limit) {
+  // productsContainer.innerHTML = "";
 
+  console.log("pageNo-1:", pageNo);
+
+  let skip = (pageNo - 1) * limit;
+
+  const products = await getSearchProducts(searchText, skip, limit);
+
+  totalItems = products.total;
+
+  if (skip + limit > totalItems) {
+    hasMoreData = false;
+  }
+
+  displayProducts(products.products, productsContainer);
+}
 
 // TODO:This commented code will be removed later
 
